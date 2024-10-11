fca9d1d8c07ec093f17faf5ac66587fa094884ab
Amine Bensaoucha <bensaoucha84@gmail.com>
2024-10-12 01:57:43 +0200

    reducer redux

--- src/reducers/user.reducer.js
+++ src/reducers/user.reducer.js
@@ -6,6 +6,7 @@ export default function userReducer(state = initialState, action) {
     switch (action.type) {
         case GET_USER:
             return action.payload;
+        // likes
         case ADD_USER_LIKE:
             return {
                 ...state,
